48c55df49beaa7e4ac0589004793f66aa85538ac
Erich Moraga <33645296+ErichMoraga@users.noreply.github.com>
2021-12-15 09:20:36 -0600

    Circumvented custom fork warning

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -17,7 +17,7 @@ def get_startup_event(car_recognized, controller_available, fw_seen):
   if get_comma_remote() and get_tested_branch():
     event = EventName.startup
   else:
-    event = EventName.startupMaster
+    event = EventName.startup
 
   if not car_recognized:
     if fw_seen:
